7e9667b9a307b97e6f19cac0fdd32cd694c8a991
liyageorg <liyageorge009@gmail.com>
2025-01-21 10:40:13 +0530

    second

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-//import search_icon from '../../assets/search_icon.svg'
-//import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
-//import caret_icon from '../../assets/caret_icon.svg'
-
-
-
+import React from 'react';
+import './Navbar.css';
+import { Search, Bell } from 'lucide-react';
+import logo from '../../assets/logo.png';
+import profile_img from '../../assets/profile_img.png';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-   <div className="navbar-left">
-    <img src={logo} alt="logo"/>
-    <ul>
-      <li>Home</li>
-      <li>Tv Shows</li>
-      <li>Movies</li>
-      <li>Recently Added</li>
-      <li>My List</li>
-      
-    </ul>
-   </div>
-   <div className="navbar-right">
-    <p>Kids</p>
-    <p>DVD</p>
-     <div className="navbar-profile">
-     <img src={profile_img} alt="" className='profile'/>
-     <div className="dropdown">
-      <p>Sign Out of Netflix</p>
-     </div>
-     </div>
-   </div>
-   </div>
-  )
-}
+      <div className="navbar-left">
+        <img src={logo} alt="Netflix Logo" />
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>Recently Added</li>
+          <li>My List</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        {/* Search Icon */}
+        <div className="navbar-search">
+          <Search size={20} className="icon" />
+        </div>
 
-export default Navbar
+        {/* Kids Section */}
+        <p>Kids</p>
+
+        {/* Notification Icon */}
+        <div className="navbar-notification">
+          <Bell size={20} className="icon" />
+        </div>
+
+        {/* Profile with Dropdown */}
+        <div className="navbar-profile">
+          <img src={profile_img} alt="Profile" className="profile" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
